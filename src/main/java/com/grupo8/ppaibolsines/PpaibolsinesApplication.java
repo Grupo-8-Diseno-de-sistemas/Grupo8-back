6475f10846c7012e77f6fe484a6bf1b1f59ce4f7
agustin-123 <agustinsorti@gmail.com>
2026-06-19 10:17:14 -0300

    Inicializacion proyecto

---
+++ src/main/java/com/grupo8/ppaibolsines/PpaibolsinesApplication.java
@@ -0,0 +1,13 @@
+package com.grupo8.ppaibolsines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpaibolsinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpaibolsinesApplication.class, args);
+	}
+
+}
